a1ab47fe5d6341e1e97d788cc8090b2fd1ed1044
williamchnt <chenut.william@gmail.com>
2023-09-13 11:43:09 +0200

    Update hours-count.py
    - change to time_input
    - Add hours min

--- hours-count.py
+++ hours-count.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from datetime import datetime, timedelta
+from datetime import datetime,timedelta,time
 
 def setIconPage():
     st.set_page_config(
@@ -21,32 +21,39 @@ def HideStreamlitContent():
 
 def data():
     col1, col2, col3 = st.columns(3)
-    arrivalHours = col1.number_input("Heure d'arriver :",8,14,step=1)
-    arrivalMin = col1.number_input("minutes d'arriver :",0,60,step=1)
-    startDejHours = col2.number_input("Heure du déjeuner :",11,14,step=1)
-    startDejMin = col2.number_input("minutes du déjeuner :",0,60,step=1)
-    EndDejHours = col3.number_input("Heure fin de déjeuner :",12,14,step=1)
-    EndDejMin = col3.number_input("Minutes fin de déjeuner :",0,60,step=1)
+    arrival = col1.time_input("Heure d'arrivée", time(8, 45))
+    startDej = col2.time_input("Heure de déjeuné", time(12, 30))
+    EndDej = col3.time_input("Heure de fin de déjeuné", time(13, 30))
+    # arrivalHours = col1.number_input("Heure d'arriver :",8,14,step=1)
+    # arrivalMin = col1.number_input("minutes d'arriver :",0,60,step=1)
+    # startDejHours = col2.number_input("Heure du déjeuner :",11,14,step=1)
+    # startDejMin = col2.number_input("minutes du déjeuner :",0,60,step=1)
+    # EndDejHours = col3.number_input("Heure fin de déjeuner :",12,14,step=1)
+    # EndDejMin = col3.number_input("Minutes fin de déjeuner :",0,60,step=1)
+    hoursToDo = col1.time_input("Heure minimum à réaliser", time(7, 36))
+    st.divider()
+    FMT = '%H:%M:%S'
 
-    FMT = '%H:%M'
+    # arrival = str(int(arrivalHours))+":"+str(int(arrivalMin))
+    arrival = datetime.strptime(str(arrival), FMT)
 
-    arrival = str(int(arrivalHours))+":"+str(int(arrivalMin))
-    arrival = datetime.strptime(arrival, FMT)
+    # startDej = str(int(startDejHours))+":"+str(int(startDejMin))
+    startDej = datetime.strptime(str(startDej), FMT)
 
-    startDej = str(int(startDejHours))+":"+str(int(startDejMin))
-    startDej = datetime.strptime(startDej, FMT)
+    # EndDej = str(int(EndDejHours))+":"+str(int(EndDejMin))
+    EndDej = datetime.strptime(str(EndDej), FMT)
 
-    EndDej = str(int(EndDejHours))+":"+str(int(EndDejMin))
-    EndDej = datetime.strptime(EndDej, FMT)
-
+    hoursToDo = datetime.strptime(str(hoursToDo), FMT)
+    
         
     morning = startDej - arrival
 
+
     st.subheader("Temps le matin : "+str(morning))
 
-    timeToFinish = datetime.strptime("7:36", FMT) - morning
+    timeToFinish = hoursToDo - morning
 
-    st.subheader("Heure restante l'après-midi : "+str(timeToFinish)[11:])
+    st.subheader("Temps restant l'après-midi : "+str(timeToFinish)[11:])
 
     hoursToAdd = str(timeToFinish)[11:13]
     minutesToAdd = str(timeToFinish)[14:16]
